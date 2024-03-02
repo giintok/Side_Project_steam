9fd05e8bb14d058d3003ebd7a1b7192117893a67
김기인 <140138067+giintok@users.noreply.github.com>
2024-03-02 19:02:27 +0900

    Update utils_recommendation_by_tags_select.py

--- utils/utils_recommendation_by_tags_select.py
+++ utils/utils_recommendation_by_tags_select.py
@@ -15,7 +15,7 @@ basic_path = os.path.dirname(__file__)
 basic_file_path = os.path.join(basic_path, 'data')
 
 # secrets.json 파일의 상대 경로
-secrets_file_path = os.path.join(script_dir, 'secrets.json')
+secrets_file_path = os.path.join(basic_path, 'utils', 'secrets.json')
 
 # 게임 파일 경로
 file_path = os.path.join(basic_file_path, 'final_dataset.csv')
